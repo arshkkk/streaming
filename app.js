95c589562e86728ee20e6f4cf983a9e481a160d3
arshdeep <arrshkumar05@gmail.com>
2020-05-03 20:38:09 +0530

    final

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const path = require('path')
 
 const app = express()
 
+app.get('/json',(req,res)=>{
+res.json({success: true})
+})
 app.get('/',(req,res)=>{
     const stream = fs.createReadStream('video.mp4')
     // res.setHeader('Content-Type', 'video')
     res.header({'Content-Type':'video/mp4'})
    	res.sendFile(path.join(__dirname,'video.mp4'))
+   	
 })
 
 app.listen(3000)
