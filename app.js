e5d8a1698c8b9976d8f0724c6b625e975b5a4f5e
arshdeep <arrshkumar05@gmail.com>
2020-05-06 04:15:21 +0530

    adkf

--- app.js
+++ app.js
@@ -24,13 +24,14 @@ app.get('/',(req,res,next)=>{
 
 	res.sendFile(path.join(__dirname,'video.mp4'))
 })
+
 app.get('/download',(req,res,next)=>{
   const Fs = require('fs')  
 const Path = require('path')  
 const Axios = require('axios')
 
 async function downloadImage () {  
-  const url = 'https://r6---sn-ci5gup-pmjd.googlevideo.com/videoplayback?expire=1588739503&ei=T-mxXtOkAcyPkwbnj5noAQ&ip=64.145.94.137&id=o-ACeGyl9ErhTdaX8xQ8JOfxtJtjVDLCrm8_mt1NrEaMoC&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&gir=yes&clen=462097498&ratebypass=yes&dur=8892.963&lmt=1586975654091121&fvip=6&c=WEB&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AJpPlLswRQIhAJaevP-R6BXQMSfUawkrGOc0s2fh737wX_CfFc2yb0UqAiApwnLAYGtyT6U2RvlPf5stP1xk1Y9K7aNaclxJeGb1Ug%3D%3D&video_id=kSep4hMsE1A&title=Shahrukh+%26+Alia+Bhatt+Superhit+Movie&redirect_counter=1&rm=sn-a5mk67d&req_id=d21452ed9fa4a3ee&cms_redirect=yes&ipbypass=yes&mh=jZ&mip=223.185.138.155&mm=31&mn=sn-ci5gup-pmjd&ms=au&mt=1588717751&mv=u&mvi=5&pl=22&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=ALrAebAwRQIhAJ-OWpBVDsk2Y8Zb5zVxWBioNb7zd-ARuL80OreQav0wAiBKJS2BtIamD7JP5J54TimkIs_0eD99LIBTRzZNhawBlA%3D%3D'
+  const url ='https://r1---sn-ci5gup-qxaee.googlevideo.com/videoplayback?expire=1588740201&ei=CeyxXr7SC53IyQWel6m4Dg&ip=77.65.13.85&id=o-AEkqw7QS3zIegHFLloQFyi7T78vXn9gP5e7uyi01mAvN&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&gir=yes&clen=39081439&ratebypass=yes&dur=856.816&lmt=1575012543500748&fvip=1&fexp=23882513&c=WEB&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AJpPlLswRgIhAO_a4HwfF_BA1oR5WtYuoPu9SdCYd08FvzANugDkfFHJAiEAtGBhsfcGewNGJinXLJXsi0V6jEil42uwYTzlQSCwLV4%3D&video_id=LWVszQoVnGY&title=Hindi+Touching+Short+Film+-+Baalak+-+An+emotional+drama+filled+with+sentiments+%26+emotions&rm=sn-pj2-2v1s7e,sn-f5fr7e&req_id=52ba9018c872a3ee&redirect_counter=2&cms_redirect=yes&ipbypass=yes&mh=wQ&mip=223.185.138.155&mm=29&mn=sn-ci5gup-qxaee&ms=rdu&mt=1588718491&mv=m&mvi=0&pl=22&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAKDrFFvRJkXqM3Ot0j4RLc3ZDwmp2SWrIqIhV0gRRStVAiBlGQ3aBh5Bu4qDBc9YmtV2_PxnIK2Z3aSb-JEoVSZwSg%3D%3D'
   const path = Path.resolve(__dirname, 'videos', 'technical.mp4')
   const writer = Fs.createWriteStream(path)
 
