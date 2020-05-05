e3ed78a3ca1644dbf808cc9719d6dc11136cb1bd
arshdeep <arrshkumar05@gmail.com>
2020-05-05 12:55:13 +0530

    changed video.mp4

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const path = require('path')
 
 const app = express()
 
-app.get('/json',(req,res)=>{
-res.json({success: true})
-})
+
 app.get('/',(req,res)=>{
-    const stream = fs.createReadStream('video.mp4')
+    //const stream = fs.createReadStream('video.mp4')
     // res.setHeader('Content-Type', 'video')
     res.header({'Content-Type':'video/mp4'})
    	res.sendFile(path.join(__dirname,'video.mp4'))
    	
 })
 
-app.listen(3000)
+app.listen(3002)
