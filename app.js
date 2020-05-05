bef5dca24936d3fd62b8b595fadb00918cd4d565
arshdeep <arrshkumar05@gmail.com>
2020-05-05 13:09:09 +0530

    express

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express()
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
+var app = express();
 
-app.get('/',(req,res)=>{
-    //const stream = fs.createReadStream('video.mp4')
-    // res.setHeader('Content-Type', 'video')
-    res.header({'Content-Type':'video/mp4'})
-   	res.sendFile(path.join(__dirname,'video.mp4'))
-   	
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',(req,res,next)=>{
+
+	res.sendFile(path.join(__dirname+'/video.mp4'))
 })
 
-app.listen(3002)
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
